2beab957b9c8dd6b08236a78c4113281fad215e4
Jeypestana <jeysson.pestana@gmail.com>
2023-06-05 12:57:57 -0500

    modelo logistico con datos desbalanceados, aplicado a creditos en mora con 3 metodos de sub-muestreo

--- modelo.r
+++ modelo.r
@@ -7,8 +7,6 @@ library(caret)
 data <- read_excel("Policiauniformado_modelo.xlsx")
 head(data)
 
-
-
 # eliminamos las varibales sin relevancia para el modelo
 colnames(data)
 mode1 <- data[, c(-1,-16,-17,-19)]
@@ -24,24 +22,6 @@ mode1$ingresos_reportados[is.na(mode1$ingresos_reportados)] <-
 table(mode1$mora) # Good = No / Bad = si
 # valores en proporcion
 prop.table(table(mode1$mora)) *100
-
-
-#renombramos los malos como "0" y buenos como "1"
-#mode1$mora <- ifelse(mode1$mora=='Si',0,1)
-#table(mode1$mora) # Good = 1 / Bad = 0
-#prop.table(table(mode1$mora)) *100
-
-
-#convertimos variables de tipo categoricas a factor
-#factor(mode1$genero)
-#factor(mode1$actividad_cliente)
-#factor(mode1$estado_civil)
-#factor(mode1$tipo_vivienda)
-#factor(mode1$regional)
-#factor(mode1$oficina)
-#factor(mode1$departamento_cliente)
-#factor(mode1$linea_credito)
-
 
 #realizamos un Cross-Validation "variacion de parametros
 set.seed(1234)
@@ -62,10 +42,6 @@ train <- createDataPartition(mode1$mora, p= 0.7 , list = FALSE)
 mode1.test <- mode1[-train,]
 mode1.train <- mode1[train,]
 
-#ytrain <- mode1$mora[train]
-#ytest <- mode1$mora[-train]
-
-
 # Revisamos las proporciones entre el inicial el test y el train
 prop.table(table(mode1$mora)) *100 # proporcion base inicial
 prop.table(table(mode1.train$mora)) *100 # proporcion entreno
@@ -76,7 +52,7 @@ mode1.train$mora <- ifelse(mode1.train$mora== "Si" ,1,0)
 
 #recodificamos la variable dependiente en factor de dos niveles con etiquetas
 #para el sub-muistreo
-mode1.train$mora <- recode(mode1.train$mora, "1=1 ;0=0")
+#mode1.train$mora <- recode(mode1.train$mora, "1=1 ;0=0")
 table(mode1.train$mora) # al dia = 0 / mora = 1
 mode1.train$mora <- as.factor(mode1.train$mora) # convertimos como factor
 mode1.train$mora = factor(mode1.train$mora, 
@@ -88,7 +64,6 @@ str(mode1.train$mora) # revisamos los cambios factos dos niveles con labels
 
 # teniendo en cuenta la desproporcion de los datos realizamos muestreos
 # Down sub-muestreo
-
 set.seed(1234)
 dow_train <- downSample(x = mode1.train[,-16], y = mode1.train$mora)
 table(dow_train$Class) # proporcion test igual
@@ -98,7 +73,7 @@ set.seed(1234)
 up_train <- upSample(x = mode1.train[,-16], y = mode1.train$mora)
 table(up_train$Class) # proporcion test igual)
 
-#revisamos varibales que apliquen cambio a factor
+#revisamos varibales que apliquen cambio a factor para el muestreo de ROSE
 str(mode1.train)
 
 mode1.train <- mode1.train %>% mutate(genero = factor(genero), actividad_cliente = factor(actividad_cliente),
@@ -113,7 +88,7 @@ rose_train <- ROSE(mora ~ ., data = mode1.train)$data
 table(rose_train$mora)
 
 # muestreo SMOTE
-
+#revisamos las varibales que apliquen cambio a numeric
 str(mode1.train)
 
 mode1.train <- mode1.train %>% mutate(genero = as.numeric(genero), actividad_cliente = as.numeric(actividad_cliente),
@@ -121,64 +96,87 @@ mode1.train <- mode1.train %>% mutate(genero = as.numeric(genero), actividad_cli
                                       regional = as.numeric(regional) , linea_credito = as.numeric(linea_credito))
 
 
-library(smotefamily)
-set.seed(1234)
-smote_train <- SMOTE(mode1.train[,-16],as.numeric(mode1.train$mora))
-smote_train <- smote_train$data
-table(smote_train$class)
+# Estimacion de metricas de modelos
+
+#DOWN
+#generamos vectores para especifidad , sensitividad y ROC
+sen_d <- rep(0,10)
+esp_d <- rep(0,10)
+roc_d <- rep(0,10)
+#valores de control del modelos "library caret
+control <- trainControl(method = "cv", number = 5, classProbs = TRUE,
+                        summaryFunction = twoClassSummary)
+# bucle 10 para almacenar los resultados 
+for (i in 1:10) {
+  fit_dow <- train(Class ~ ., data = dow_train, method = "glm", metric = "ROC", trControl = control)
+  ypred <- predict(fit_dow,mode1.test)
+  ypred=as.factor(ypred)
+  resul=confusionMatrix(ypred,mode1.test$mora,positive= "Si")
+  roc_d[i]=as.numeric(fit_dow$results[2])
+  sen_d[i]=as.numeric(fit_dow$results[3])
+  esp_d[i]=as.numeric(fit_dow$results[4])
+}
+
+# clasificacion de los que tienen mora "exito"
+sen_d
+esp_d
+roc_d
+
+#UP
+#generamos vectores para especifidad , sensitividad y ROC
+sen_u <- rep(0,10)
+esp_u <- rep(0,10)
+roc_u <- rep(0,10)
+#valores de control del modelos "library caret
+control <- trainControl(method = "cv", number = 5, classProbs = TRUE,
+                        summaryFunction = twoClassSummary)
+# bucle 10 para almacenar los resultados 
+for (i in 1:10) {
+  fit_up <- train(Class ~ ., data = up_train, method = "glm", metric = "ROC", trControl = control)
+  ypred <- predict(fit_up,mode1.test)
+  ypred=as.factor(ypred)
+  resul=confusionMatrix(ypred,mode1.test$mora,positive= "Si")
+  roc_u[i]=as.numeric(fit_up$results[2])
+  sen_u[i]=as.numeric(fit_up$results[3])
+  esp_u[i]=as.numeric(fit_up$results[4])
+}
+
+# clasificacion de los que tienen mora "exito"
+sen_u
+esp_u
+roc_u
 
 
 
+#ROSE
+#generamos vectores para especifidad , sensitividad y ROC
+sen_r <- rep(0,10)
+esp_r <- rep(0,10)
+roc_r <- rep(0,10)
+#valores de control del modelos "library caret
+control <- trainControl(method = "cv", number = 5, classProbs = TRUE,
+                        summaryFunction = twoClassSummary)
+# bucle 10 para almacenar los resultados 
+for (i in 1:10) {
+  fit_rose <- train(mora ~ ., data = rose_train, method = "glm", metric = "ROC", trControl = control)
+  #ypred <- predict(fit_rose,mode1.test)
+  #ypred=as.factor(ypred)
+  #resul=confusionMatrix(ypred,mode1.test$mora,positive= "Si")
+  roc_r[i]=as.numeric(fit_rose$results[2])
+  sen_r[i]=as.numeric(fit_rose$results[3])
+  esp_r[i]=as.numeric(fit_rose$results[4])
+}
 
+# clasificacion de los que tienen mora "exito"
+sen_r
+esp_r
+roc_r
 
+#resultados 3 modelos de sub-muestreo
 
-
-#convertimos variables de tipo categoricas a factor
-#factor(mode1$genero)
-#factor(mode1$actividad_cliente)
-#factor(mode1$estado_civil)
-#factor(mode1$tipo_vivienda)\
-#factor(mode1$regional)
-#factor(mode1$oficina)
-#factor(mode1$departamento_cliente)
-#factor(mode1$linea_credito)
-#factor(mode1$mora)
-
-
-
-# generamos modelo con las variables sin contar numero radicacion
-# ya que esta es el identificador para cada credito
-m1 <- glm(mora ~ . -numero_radicacion -oficina, family = binomial , mode1 )
-summary(m1)
-
-#varibales significaticas al 10%
-sig.var<- summary(m1)$coeff[-1,4] <0.01
-names(sig.var)[sig.var == TRUE]
-
-
-# revisamos el entrenamiento del modelo con el 30% separado
-pred1<- predict.glm(m1,newdata = mode1.test, type="response")
-# revisamos la matriz de confusion
-result1<- table(ytest, floor(pred1+0.45))
-result1
-
-# debido a la falta de prediccion , evaluamos seleccion de variables
-
-mode2 <- glm(mora ~ . -numero_radicacion -oficina, family = binomial , mode1 )
-AIC(mode2)
-
-
-# Eliminar la variable con el valor p más alto (menos significativa) en el modelo y repetir esto hasta que todas sean significativas
-modelo_res <- step(mode2, direction = "backward")
-
-# Construir un nuevo modelo sin las variables eliminadas y verificar sus metricas de ajuste
-summary(modelo_res)
-
-# revisamos las predicciones con este nuevo modelo despues de la seleccion de variables
-pred1<- predict.glm(modelo_res,newdata = mode1.test, type="response")
-# revisamos la matriz de confusion
-result1<- table(ytest, floor(pred1+0.3))
-result1
-
-
-
+#calsificar bien a los clientes en mora sensitividad
+boxplot(sen_d,sen_r,sen_u)
+#Especificidad modelo 2 clasifica mejor a los que si tiene mora
+boxplot(esp_d,esp_r,esp_u)
+#vr curva roc metodo 2 mayor presencia
+boxplot(roc_d,roc_r,roc_u)
